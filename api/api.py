1790b8d1c84fc9e48d6375d2f5880e496c623953
Chen <jayden.chen@sap.com>
2018-11-26 09:11:26 -0500

    Added backend swagger

--- api/api.py
+++ api/api.py
@@ -2,33 +2,12 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-class Tnitial(Resource):
-    def get():
-        """
-        Initial contact point of the backend service
-        """
-        return 'Hello, welcome to signature authentication with Machine Learning'
-
-@app.route('/datasets/<datasetId>')
-class Datasets(Resource):
-    def get(datasetId, **kwargs):
-        """
-        Get the dataset information that was uploaded previously
-        """
-        pass
-
-    def post(datasetId, **kwargs):
-        """
-        Post the training dataset to the backend and object store
-        """
-        pass
+def hello_world():
+    return 'Hello There'
 
 @app.route('/process')
-    def post(**kwargs):
-        """
-        Upload the signatures for processing
-        """
-        pass
+def upload():
+    return "Submit an invoice for process"
 
-if __name == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+if __name__ == '__main__':
+    app.run(debug=True,host='0.0.0.0')
