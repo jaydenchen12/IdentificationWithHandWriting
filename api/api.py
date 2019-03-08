0cae16f9ea78ed47e5e24721a6dbbeea96b1b7b4
Dhruv Patel <dap332@drexel.edu>
2019-03-08 18:29:24 -0500

    add CORS header

--- api/api.py
+++ api/api.py
@@ -1,7 +1,9 @@
 from flask import Flask
+from flask_cors import CORS
 from apis import api
 
 app = Flask(__name__)
+cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
 api.init_app(app)
 
 app.run(debug=True)
