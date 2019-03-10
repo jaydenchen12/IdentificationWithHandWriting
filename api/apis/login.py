71aac97813971c14b20e1283bfb2bd588e2d8fc9
Anthony Montefiore <anthmont188@gmail.com>
2019-03-10 18:23:23 -0400

    Modified the login.py bay accident

--- api/apis/login.py
+++ api/apis/login.py
@@ -10,7 +10,4 @@ login_parser.add_argument('password', required=True)
 @api.expect(login_parser)
 class Login(Resource):
     def post(self):
-        response = Flask.jsonify({'token': 'abdcefg_test_token'})
-        response.headers.add('Access-Control-Allow-Origin', 'True')
-        return response, 200
-        # return {'token': 'abdcefg_test_token'}, 200
+        return {'token': 'abdcefg_test_token'}, 200
