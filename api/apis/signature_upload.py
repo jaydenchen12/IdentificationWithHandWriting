29fc6e0c1e3586d48d736b556005e8b4a54a1f5e
LAPTOP-K7QL8DP0\Default <dap332@drexel.edu>
2019-03-10 22:44:23 -0400

    save file

--- api/apis/signature_upload.py
+++ api/apis/signature_upload.py
@@ -2,6 +2,12 @@ from flask_restplus import Namespace, Resource, fields
 from flask import request
 from werkzeug.datastructures import FileStorage
 
+import sys
+sys.path.insert(0, '../model/test_signature.py')
+
+
+import test_signature
+
 api = Namespace('Signature', description='Process')
 
 
@@ -16,8 +22,11 @@ upload_parser.add_argument('user_name', required=True)
 class Signature(Resource):
     def post(self):
       #  uploaded_file = args['file']  # This is FileStorage instance
-        uploaded_file = request.files
-        print('files:', uploaded_file)
+        uploaded_file = request.files['file']
+        print('files:', type(uploaded_file))
+        uploaded_file.save('./signature.png')
+       # with open('signature.png', 'wb') as f:
+       #     f.write(uploaded_file['file'])
         #url = do_something_with_file(uploaded_file)
         return {'record_id': '1234testing'}, 201
 
