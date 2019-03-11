8bf5a75b8abf7e8fabb94f4a64b69286b8e9e2d4
Anthony Montefiore <anthmont188@gmail.com>
2019-03-10 20:17:28 -0400

    Upating UI for Rest Call Debug

--- SigAuthUI/src/main/controllers/signatureController.js
+++ SigAuthUI/src/main/controllers/signatureController.js
@@ -1,5 +1,115 @@
+//  ------------------------------------------- Save as image ---------------------------------------------------- //
 
-//  ----------------------------------------------- Canvas ------------------------------------------------------- //
+function saveAsPNG(){
+    var canvas = document.getElementById("myCanvas");
+
+    // var newCanvas = cloneCanvas(canvas);
+    // var imgDiv = document.getElementById("imageDiv");
+    // imgDiv.appendChild(newCanvas);
+
+    var img = canvas.toDataURL("image/png");
+
+    var formData = new FormData(img);
+    formData.append('file', dataURLtoBlob(img));
+
+    // Ajax call to hit the rest call for uploading signatures
+    $.ajax({
+      type: 'POST',
+      url:"http://localhost:5000/Signature/verify_signature/?token=asd&username=asd",
+      enctype: 'multipart/form-data',
+      processData: false,
+      contentType: false,
+      cache: false,
+      data: formData,
+        //Cross Origin Support
+      cors: true,
+      headers: {
+         'Access-Control-Allow-Origin': '*'
+      },
+      success: function(msg){
+          alert('PNG has successfully been uploaded!');
+      },
+      error: function(xhr, ajaxOptions, thrownError){
+          alert('Error contacting server!');
+      }
+    });
+
+    //Fake return message to users
+    alert('PNG has successfully been uploaded!');
+
+}
+
+function dataURLtoBlob(dataurl) {
+    var arr = dataurl.split(','), mime = arr[0].match(/:(.*?);/)[1],
+        bstr = atob(arr[1]), n = bstr.length, u8arr = new Uint8Array(n);
+    while(n--){
+        u8arr[n] = bstr.charCodeAt(n);
+    }
+    return new Blob([u8arr], {type:mime});
+}
+
+$(document).ready(function () {
+
+    $("#submitBtn").click(function (event) {
+
+        //stop submit the form, we will post it manually.
+        event.preventDefault();
+
+        var form = $('#fileUploadForm')[0];
+
+        // Create an FormData object
+        var data = new FormData(form);
+
+
+        // disabled the submit button
+        $("#submitBtn").prop("disabled", true);
+
+        $.ajax({
+            type: "POST",
+            enctype: 'multipart/form-data',
+            url:"http://localhost:5000/Signature/verify_signature/?token=asd&user_name=asd",
+            data: data,
+            processData: false,
+            contentType: false,
+            cache: false,
+            success: function (data) {
+                console.log("SUCCESS : ", data);
+                $("#submitBtn").prop("disabled", false);
+
+            },
+            error: function (e) {
+                console.log("ERROR : ", e);
+                $("#submitBtn").prop("disabled", false);
+
+            }
+        });
+
+    });
+
+});
+
+function cloneCanvas(oldCanvas) {
+
+    //create a new canvas
+    var newCanvas = document.createElement('canvas');
+    var newContext = newCanvas.getContext('2d');
+
+    //set dimensions
+    newCanvas.width = oldCanvas.width;
+    newCanvas.height = oldCanvas.height;
+
+    //apply the old canvas to the new one
+    newContext.drawImage(oldCanvas, 0, 0);
+
+    //return the new canvas
+    return newCanvas;
+}
+
+//----------------------------------------^^^^ Save as image ^^^^-----------------------------------------------------//
+
+
+
+//----------------------------------------------- Canvas -------------------------------------------------------------//
 var canvas = document.getElementById("myCanvas");
 var context = document.getElementById("myCanvas").getContext("2d");
 
@@ -68,7 +178,20 @@ function redraw(){
     }
 }
 
-//-------------------------- Mobile Support -------------------------------//
+//  ------------------------------------------- Clear Canvas ----------------------------------------------------- //
+
+function reset() {
+    var canvas= document.getElementById('myCanvas');
+    var ctx = canvas.getContext('2d');
+    clickX = new Array();
+    clickY = new Array();
+    clickDrag = new Array();
+    ctx.clearRect(0, 0,  canvas.width, canvas.height);
+}
+//-------------------------------------------^^^^ Clear Canvas ^^^^---------------------------------------------------//
+
+
+//---------------------------------------------- Mobile Support ------------------------------------------------------//
 
 // Set up touch events for mobile, etc
 canvas.addEventListener("touchstart", function (e) {
@@ -119,182 +242,10 @@ document.body.addEventListener("touchmove", function (e) {
     }
 }, false);
 
-//----------------------^^^^ Mobile Support ^^^^---------------------------//
+//-----------------------------------------^^^^ Mobile Support ^^^^---------------------------------------------------//
 
 
-//  ------------------------------------------^^^^ Canvas ^^^^-------------------------------------------------- //
-
-
-//  ------------------------------------------- Save as image ---------------------------------------------------- //
-
-function saveAsPNG(){
-    var canvas = document.getElementById("myCanvas");
-
-    var newCanvas = cloneCanvas(canvas);
-    var imgDiv = document.getElementById("imageDiv");
-    imgDiv.appendChild(newCanvas);
-
-    var img = canvas.toDataURL("image/png");
-
-    var formData = new FormData();
-    formData.append('file', dataURLtoBlob(img));
-
-    // Ajax call to hit the rest call for uploading signatures
-    $.ajax({
-      type: 'POST',
-      url:"http://localhost:5000/Signature/verify_signature/?token=asd&username=asd",
-      enctype: 'multipart/form-data',
-      processData: false,
-      contentType: false,
-      cache: false,
-      data: formData,
-        //Cross Origin Support
-      cors: true,
-      headers: {
-         'Access-Control-Allow-Origin': '*'
-      },
-      success: function(msg){
-          alert('PNG has successfully been uploaded!');
-      },
-      error: function(xhr, ajaxOptions, thrownError){
-          alert('Error contacting server!');
-      }
-    });
-
-    //Fake return message to users
-    alert('PNG has successfully been uploaded!');
-
-}
-
-function uploadPNG(evt){
-    var files = evt.target.files; // FileList object
-
-    // Loop through the FileList and render image files as thumbnails.
-    for (var i = 0, f; f = files[i]; i++) {
-
-        // Only process image files.
-        if (!f.type.match('image.*')) {
-            continue;
-        }
-
-        var reader = new FileReader();
-
-        // Closure to capture the file information.
-        reader.onload = (function(theFile) {
-            return function(e) {
-                // Render thumbnail.
-                var span = document.createElement('span');
-                span.innerHTML = ['<img class="thumb" src="', e.target.result,
-                    '" title="', escape(theFile.name), '"/>'].join('');
-                document.getElementById('imageDiv').insertBefore(span, null);
-            };
-        })(f);
-
-        // Read in the image file as a data URL.
-        reader.readAsDataURL(f);
-        var formData = new FormData();
-        formData.append('uploadedPNG', dataURLtoBlob(reader.result));
-        $.ajax({
-            type: "POST",
-            enctype: 'multipart/form-data',
-            processData: false,  // Important!
-            contentType: false,
-            cache: false,
-            url:"http://localhost:5000/Signature/verify_signature/?token=asd&username=asd",
-            data: formData,
-            //Cross Origin Support
-            cors: true,
-            headers: {
-                'Access-Control-Allow-Origin': '*'
-            },
-            success: function(msg){
-                alert('PNG has successfully been uploaded!');
-            },
-            error: function(xhr, ajaxOptions, thrownError){
-                alert('Error contacting server!');
-            }
-        });
-    }
-}
-
-function dataURLtoBlob(dataurl) {
-    var arr = dataurl.split(','), mime = arr[0].match(/:(.*?);/)[1],
-        bstr = atob(arr[1]), n = bstr.length, u8arr = new Uint8Array(n);
-    while(n--){
-        u8arr[n] = bstr.charCodeAt(n);
-    }
-    return new Blob([u8arr], {type:mime});
-}
-
-function cloneCanvas(oldCanvas) {
-
-    //create a new canvas
-    var newCanvas = document.createElement('canvas');
-    var newContext = newCanvas.getContext('2d');
-
-    //set dimensions
-    newCanvas.width = oldCanvas.width;
-    newCanvas.height = oldCanvas.height;
-
-    //apply the old canvas to the new one
-    newContext.drawImage(oldCanvas, 0, 0);
-
-    //return the new canvas
-    return newCanvas;
-}
-
-//  ------------------------------------------- Save as image ---------------------------------------------------- //
+//  ------------------------------------------^^^^ Canvas ^^^^--------------------------------------------------------//
 
 
 
-//  ------------------------------------------- Clear Canvas ----------------------------------------------------- //
-
-function reset() {
-    var canvas= document.getElementById('myCanvas');
-    var ctx = canvas.getContext('2d');
-    clickX = new Array();
-    clickY = new Array();
-    clickDrag = new Array();
-    ctx.clearRect(0, 0,  canvas.width, canvas.height);
-}
-//  ------------------------------------------- Clear Canvas ----------------------------------------------------- //
-
-$(document).ready(function () {
-
-    $("#submitBtn").click(function (event) {
-
-        //stop submit the form, we will post it manually.
-        event.preventDefault();
-
-        var form = $('#fileUploadForm')[0];
-
-        // Create an FormData object
-        var data = new FormData(form);
-
-
-        // disabled the submit button
-        $("#submitBtn").prop("disabled", true);
-
-        $.ajax({
-            type: "POST",
-            enctype: 'multipart/form-data',
-            url:"http://localhost:5000/Signature/verify_signature/?token=asd&user_name=asd",
-            data: data,
-            processData: false,
-            contentType: false,
-            cache: false,
-            success: function (data) {
-                console.log("SUCCESS : ", data);
-                $("#submitBtn").prop("disabled", false);
-
-            },
-            error: function (e) {
-                console.log("ERROR : ", e);
-                $("#submitBtn").prop("disabled", false);
-
-            }
-        });
-
-    });
-
-});
