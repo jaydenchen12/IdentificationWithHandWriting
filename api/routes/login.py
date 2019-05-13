57ede2a5b01ea101ad28a729f04794cda64ebc36
Chen <jayden.chen@sap.com>
2019-05-13 19:23:59 -0400

    added working mongo code

--- api/routes/login.py
+++ api/routes/login.py
@@ -1,6 +1,16 @@
 from flask_restplus import Namespace, Resource, fields
 from werkzeug.datastructures import FileStorage
-from flask import Flask
+from werkzeug.utils import secure_filename
+from flask import Flask, request
+from flask_cors import CORS
+# from pymongo.binary import Binary
+import pymongo
+import datetime
+UPLOAD_FOLDER = '/objectstore'
+
+app = Flask(__name__)
+mongoCilent = pymongo.MongoClient("mongodb://mongodb:27017/")
+mongo = mongoCilent["sigml"]
 
 api = Namespace('Login', description='Authication')
 login_parser = api.parser()
@@ -10,14 +20,38 @@ login_parser.add_argument('password', required=True)
 @api.expect(login_parser)
 class Login(Resource):
     def post(self):
-        return {'token': 'abdcefg_test_token'}, 200
+        user = mongo.db.users.find_one({'username':  request.args.get('username')})
+        if user['password'] == request.args.get('password'):
+            return {'token': 'abdcefg_test_token'}, 200
+        else:
+            return "Wrong Password"
 
 
 create_parser = api.parser()
 create_parser.add_argument('username', required=True)
 create_parser.add_argument('password', required=True)
+create_parser.add_argument('file_0', location='files',
+                           type=FileStorage, required=True)
+create_parser.add_argument('file_1', location='files',
+                           type=FileStorage, required=True)
+create_parser.add_argument('file_2', location='files',
+                           type=FileStorage, required=True)
 @api.route('/create_tenant')
 @api.expect(create_parser)
-class Login(Resource):
+class Signup(Resource):
     def post(self):
+        file0 = request.files['file_0'].read()
+        # file1 = request.files['file_1']
+        # file2 = request.files['file_2']
+        # if files:
+        #     for file in files:
+        #         filename = secure_filename(file.filename)
+        #         file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        #         secure_file_names.append(filename)
+        result = mongo.db.users.insert_one(
+            {'username':  request.args.get('username'),
+             'password': request.args.get('password'),
+             'create_time': datetime.datetime.utcnow(),
+             'ground_truth_1': file0})
+
         return "User Created!", 201
--- api/routes/signature_upload.py
+++ api/routes/signature_upload.py
@@ -1,11 +1,18 @@
+
 from flask_restplus import Namespace, Resource, fields
-from flask import request
+from flask import request, Flask
 from werkzeug.datastructures import FileStorage
-
-from . import producer
+from werkzeug.utils import secure_filename
+from pymongo import MongoClient
 import sys
+import datetime
+sys.path.append('../model/')
+client = MongoClient('localhost', 27017)
+db = client['sig_ml']
+UPLOAD_FOLDER = '/objectstore'
 
-
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 api = Namespace('Signature', description='Process')
 
@@ -20,12 +27,23 @@ upload_parser.add_argument('user_name', required=True)
 @api.expect(upload_parser)
 class Signature(Resource):
     def post(self):
-      #  uploaded_file = args['file']  # This is FileStorage instance
-
-        uploaded_file = request.files['files']
-        print("received")  
-        producer.produce_msg("42069")    
-        return "Image ID is: 42069", 201
+        files = request.files['files']
+        secure_file_names = []
+        if files:
+            file = files[0]
+            secure_file_name = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], secure_file_name))
+        record_id = db.jobs.InsertOneResult(
+            {'user':  request.args.get('username'),
+             'status': 'in_progress',
+             'upload_time': datetime.datetime.utcnow(),
+             'last_modified': datetime.datetime.utcnow(),
+             'confidence': 'None',
+             'authorized': 'None',
+             'signature': secure_file_name})
+        test_signature.test_image(uploaded_file)
+        
+        return record_id, 201
 
 
 get_status_parser = api.parser()
@@ -34,4 +52,13 @@ get_status_parser.add_argument('record_id', required=True)
 @api.expect(get_status_parser)
 class Process(Resource):
     def get(self):
-        return {'status': 'processing'}, 203
+        record = db.jobs.find_one({'_id':  request.args.get('record_id')})
+        if record['status'] == 'completed':
+            return {'record_id': record['_id'],
+                    'status': record['status'],
+                    'authorized': record['authorized'],
+                    'confidence': record['confidence']}, 200
+        elif record['status'] == 'in_progress':
+            return {'status': 'in_progress'}, 203
+        else:
+            return {'status': 'error'}, 204
